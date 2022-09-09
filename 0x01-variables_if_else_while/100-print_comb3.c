5b1ce4c3979c8d41ad97546ac6e9e6f9a8214697
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-09-09 15:52:37 +0000

    fix file 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,9 +7,11 @@
  */
 int main(void)
 {
-	for (int d1 = 48; d1 < '9'; ++d1)
+	int d1;
+	int d2;
+	for (d1 = 48; d1 < '9'; ++d1)
 	{
-		for (int d2 = 48; d2 <= '9'; ++d2)
+		for (d2 = 48; d2 <= '9'; ++d2)
 		{
 			if (d1 == d2)
 			{
