ea99e78a72c1d2d72b9f46c60840cf03b7caae51
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-09-09 17:23:37 +0000

    Fix file 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,15 +16,14 @@ int main(void)
 	{
 		for (d2 = 48; d2 < '9'; ++d2)
 		{
+			putchar(d1);
+			putchar(d2);
+			putchar(' ');
 			for (d3 = 48; d3 <= '9'; ++d3)
 			{
-				for (d4 = 48; d4 <= '9'; ++d4)
+				for (d4 = 48 ; d4 <= '9'; ++d4)
 				{
-					putchar(d1);
-					putchar(d2);
-					putchar(' ');
-
-					if (d3 == 0 && d4 == 0)
+					if (d4 == 48)
 						continue;
 
 					putchar(d3);
