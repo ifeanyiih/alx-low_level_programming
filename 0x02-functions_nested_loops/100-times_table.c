d3eb81b269dc5bcb9b58b1b5c77b7de8cd0d8a65
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-09-14 15:29:29 +0000

    Fix file 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+void print_numbers(int n);
+
 /**
 * print_times_table - prints the multiplication table
 * @n: integer argument
@@ -8,44 +10,48 @@ void print_times_table(int n)
 {
 	int i, j;
 
-	for (i = 0; i <= n; ++i)
+	for (i = 0; i <= n && n >= 0 && n <= 15; ++i)
 	{
 		for (j = 0; j <= n; ++j)
 		{
-			int mul;
-
-			mul = i * j;
-			if (j > 0)
-			{
-				if (mul < 10)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(mul + '0');
-					if (j < n)
-					{
-						_putchar(',');
-					}
-				}
-				else
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(mul / 10 + '0');
-					_putchar(mul % 10 + '0');
-					if (j < n)
-					{
-						_putchar(',');
-					}
-				}
-			}
+			if (j == 0)
+				_putchar('0')
 			else
-			{
-				_putchar(i * j + '0');
+				print_numbers(i * j);
+
+			if (j != n)
 				_putchar(',');
-			}
 		}
 		_putchar('\n');
+	}
+}
+
+/**
+* print_numbers - prints number into a given multiplicaiton table
+* @n: integer argument
+* Retur: void
+*/
+void print_numbers(int n)
+{
+	if (n < 10)
+	{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(n + '0');
+	}
+	else if (n < 100)
+	{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else
+	{
+		_putchar(' ');
+		_putchar(n / 100 + '0');
+		_putchar((n % 100) / 10 + '0');
+		_putchar(n % 10 + '0');
 	}
 }
