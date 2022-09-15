190dd62003745d6558039c57b3fbc2db3b332fef
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-09-15 08:22:08 +0000

    Add and Update files
    File Added: 8-print_square.c
    Updates README.md to reflect file addition
    Updates main.h to add function prototype for file

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_square - prints a square followed by a new line
+* @size: size of a side of the squre
+*/
+void print_square(int size)
+{
+	int n;
+	int m;
+
+	for (n = 1; n <= size; ++n)
+	{
+		for (m = 1; m <= size; ++m)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
