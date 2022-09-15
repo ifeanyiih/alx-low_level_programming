d2cd25d63c53ac4fb9382a31278458839bb8c3f1
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-09-15 17:27:32 +0000

    Add File

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <math.h>
+
+int is_prime(unsigned int);
+
+/**
+* main - Entry Point
+* Description: This program finds and prints the largest prime factor
+* of the number 612852475143, followed by a new line
+* Return: 0 (Success)
+*/
+int main(void)
+{
+	unsigned long int num;
+	unsigned int max_prime, i;
+
+	num = 612852475143;
+	for (i = 2; i <= floor(sqrt(num)); ++i)
+		if (is_prime(i))
+			max_prime = i;
+	printf("%u\n", max_prime);
+	return (0);
+}
+
+
+/**
+* is_prime - checks if n is prime number
+* @n: integer argument to check
+* Return: 1 (Success), 0 otherwise
+*/
+int is_prime(unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 2; i < n; i++)
+	{
+		if (n % i == 0)
+			return (0);
+	}
+
+	return (1);
+}
