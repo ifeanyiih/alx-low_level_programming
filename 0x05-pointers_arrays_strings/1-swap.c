a24803eef0e17f505ec633b320459927a882c1fa
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-09-19 16:04:06 +0000

    Add and Update files
    File Added: 1-swap.c
    Updates README.md to reflect file addition
    Updates main.h to add function prototype from file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+	#define MAIN_H "main.h"
+#endif
+#include MAIN_H
+
+/**
+* swap_int - swaps the values of two integers
+* @a: integer argument
+* @b: integer argument
+*/
+void swap_int(int *a, int *b)
+{
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
