771d6e1eba9d0cebfec1c2a678875ca6c0490013
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-09-21 09:33:17 +0000

    Add and Update files
    File Added: 2-strncpy.c
    Updates README.md to reflect file addition
    Updates main.h to add function prototype

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+	#define MAIN_H "main.h"
+#endif
+#include MAIN_H
+
+/**
+* _strncpy - copy string from src to dest
+* @dest: destination
+* @src: source
+* @n: number of characters
+* Return: a pointer to dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (n-- && (*(dest + i) = *(src + i)))
+		++i;
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -6,4 +6,4 @@ Below is a list of files in this directory, with a brief description.
 
 - **0-strcat.c**: A function that concatenates two strings
 - **1-strncat.c**: A function that concatenates two strings, using at most n bytes
-
+- **2-strncpy.c**: A function that copies a string
