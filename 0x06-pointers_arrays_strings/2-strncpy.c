9c716524e632dfe3a1da31e712326cf750ea6f1e
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-09-21 23:46:11 +0000

    modified file

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,14 +14,9 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	i = 0;
-	while (n-- && *(src + i) != '\0')
-	{
-		*(dest + i) = *(src + i);
-		++i;
-	}
-	if (n > 0)
-		while (n--)
-			*(dest + i++) = '\0';
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
