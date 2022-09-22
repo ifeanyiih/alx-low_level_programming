dd8c20a8aea03393e0334397e99193b51dfa3dfb
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-09-22 00:22:26 +0000

    modified file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,12 +15,10 @@ int _strcmp(char *s1, char *s2)
 	int i;
 
 	i = 0;
-	while (*(s1 + i) == *(s2 + i))
-	{
-		if (*(s1 + i) == '\0')
-			return (0);
+	while (*(s1 + i) == *(s2 + i) && *(s1 + i) != '\0')
 		++i;
-	}
+	if (*(s1 + i) == '\0')
+		return (0);
 	return (*(s1 + i) - *(s2 + i));
 }
 
