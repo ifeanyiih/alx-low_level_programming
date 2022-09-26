39c4f4fdceb513136bec76a06fa49b9a09997ea8
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-09-26 11:41:26 +0000

    Add and Update files
    Files Added:
    - 2-strchr.c
    
    File Updated:
    - main.h
    - README.md
    
    Updates README.md to reflect file addition
    Updates main.h to add function prototype

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _strchr - locate a character in a string
+* @s: the string
+* @c: the character
+* Return: pointer to first occurence of character
+*/
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	i = 0;
+	while (*(s + i) != c && *(s + i) != '\0')
+		++i;
+	if (*(s + i) == '\0')
+		return (0);
+	return (s + i);
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -5,4 +5,5 @@ Below are a list of files in this directory
 ---
 - **0-memset.c**: A function that fills memory with a constant byte
 - **1-memcpy.c**: A function that copies memory area.
+- **2-strchr.c**: A function that locates a character in a string.
 
