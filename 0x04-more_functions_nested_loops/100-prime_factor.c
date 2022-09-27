82f767125002af508372ccf997de4d2201388a25
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-09-27 19:01:01 +0000

    modified file

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,54 +1,18 @@
 #include <stdio.h>
-#include <math.h>
-
-int is_prime(unsigned int);
 
 /**
 * main - Entry Point
-* Description: This program finds and prints the largest prime factor
-* of the number 612852475143, followed by a new line
-* Return: 0 (Success)
+* Description: A program that prints the largest prime factor of a given number
 */
 int main(void)
 {
-	unsigned long int num;
-	unsigned int max_prime, i;
+	unsigned long int primef, i;
 
-	max_prime = 1;
-	num = 612852475143;
-	num = 9007199254740991;
-	while (!is_prime(num))
-	{
-		for (i = 3; i <= floor(sqrt(num)); ++i)
-			if (is_prime(i) && num % i == 0)
-				max_prime = i;
-		num = num / max_prime;
-		if (is_prime(num))
-			max_prime = num;
-	}
-	if (max_prime == 1)
-		printf("%lu\n", num);
-	else
-		printf("%u\n", max_prime);
-
+	primef = 612852475143;
+	primef = 9007199254740991;
+	for (i = 2; i < primef; ++i)
+		if (primef % i == 0)
+			primef /= i;
+	printf("%li\n", primef);
 	return (0);
-}
-
-
-/**
-* is_prime - checks if n is prime number
-* @n: integer argument to check
-* Return: 1 (Success), 0 otherwise
-*/
-int is_prime(unsigned int n)
-{
-	unsigned int i;
-
-	for (i = 2; i <= floor(sqrt(n)); i++)
-	{
-		if (n % i == 0)
-			return (0);
-	}
-
-	return (1);
 }
