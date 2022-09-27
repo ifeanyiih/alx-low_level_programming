7a3501da2dfa4d671272ac3f10ac9f95d16aab69
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-09-27 13:15:02 +0000

    Add and Update files
    File Added:
    - 100-rot13.c
    
    Updates README.md to reflect file addition

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+* rot13 - encodes a string using rot13
+* @s: a string
+* Return: a pointer to char
+*/
+char *rot13(char *s)
+{
+	char alpha[] = {
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+		'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+		'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+	};
+	char alpha2[] = {
+		'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+		'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
+	};
+
+	unsigned int i, j;
+
+	i = 0;
+	while (*(s + i))
+	{
+		j = 0;
+		while ( j < 52)
+		{
+			if (*(s + i) == alpha[j])
+			{
+				*(s + i) = alpha2[j];
+				break;
+			}
+			++j;
+		}
+		++i;
+	}
+	return (s);
+}
