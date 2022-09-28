0fc7fb19f15a854ca8a39997cf5af712efbf6ea7
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-09-28 22:11:49 +0000

    modified file

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+int test(int, int);
+
 /**
 * _sqrt_recursion - returns the natural square root of a
 * number
@@ -9,5 +11,26 @@
 */
 int _sqrt_recursion(int n)
 {
-	
+	if (n <= 0)
+		return (-1);
+	count = test(n - 1, 1);
+	if (count * count == n)
+		return (count);
+	return (-1);
+}
+
+
+/**
+* test - _sqrt_helper
+* @n: integer argument
+* @count: number of times recursion ran
+* Return: n count times
+*/
+int test(int n, count)
+{
+	if (n == 0)
+		return (count);
+	if (n < 0)
+		return (-1);
+	return (test(n - 2, count + 1));
 }
