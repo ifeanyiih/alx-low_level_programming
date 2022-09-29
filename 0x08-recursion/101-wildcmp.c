797d36450964e6af380f4b70c6ddb4595d6f0d8c
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-09-29 15:09:55 +0000

    modified file

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -15,7 +15,7 @@ int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-	if (*s2 == '\0' && *s2 != *s1)
+	if (*s1 == '\0' && *s2 != *s1)
 		return (0);
 	if (*s1 == *s2)
 		return (wildcmp(s1++, s2++));
@@ -25,5 +25,7 @@ int wildcmp(char *s1, char *s2)
 		return (1);
 	if (*s2 == '*' && *(s2 + 1) == *s1)
 		return (wildcmp(s1, s2++));
+	if (*s2 == '*' && *s1)
+		return (wildcmp(s1++, s2));
 	return (0);
 }
