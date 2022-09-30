c7e23a67f35b554aa75bcbb5726b65e08dd550fe
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-09-30 08:59:10 +0000

    Add and Update files
    File Added:
    - 3-mul.c
    
    File Updated:
    - README.md

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - multiplies two numbers
+* @argc: argument count
+* @argv: argument vector
+* Return: 0 (Success), 1 (otherwise)
+*/
+int main(int argc, char *argv[])
+{
+	int d1, d2, i, j;
+
+	if (argc < 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	d1 = 0, d2 = 0;
+	i = 1;
+	while (i < argc)
+	{
+		int sign, tmp;
+
+		sign = 1, tmp = 0;
+		j = 0;
+		while (argv[i][j])
+		{
+			if (argv[i][j] == '-')
+			{
+				sign *= -1;
+			}
+			else
+			{
+				tmp = tmp * 10 + (argv[i][j] - '0');
+			}
+			++j;
+		}
+		tmp *= sign;
+		if (d1 == 0)
+			d1 = tmp;
+		else
+			d2 = tmp;
+		++i;
+	}
+	printf("%d\n", d1 * d2);
+
+}
