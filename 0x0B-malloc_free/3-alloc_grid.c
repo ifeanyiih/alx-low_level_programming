a519f02b8690782b48f44dbb874849d3b0ff1f6d
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-10-03 14:34:51 +0000

    modified file

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,18 +10,30 @@
 */
 int **alloc_grid(int width, int height)
 {
-	int **arr, i, j;
+	int **arr, i, j, fail;
 
 	if (width == 0 || height == 0)
 		return (NULL);
 	arr = (int **) malloc(height * sizeof(int *));
 	if (arr == NULL)
+	{
+		free(arr);
 		return (NULL);
+	}
+	fail = 0;
 	for (i = 0; i < height; ++i)
 	{
 		arr[i] = malloc(sizeof(int) * width);
 		if (arr[0] == NULL)
-			return (NULL);
+			fail = 1;
+	}
+	if (fail == 1)
+	{
+		for (i = 0; i < height; ++i)
+		{
+			free(arr[i]);
+		}
+		free(arr);
 	}
 	for (i = 0; i < height; ++i)
 		for (j = 0; j < width; ++j)
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -8,4 +8,5 @@ Below is a list of files contained in this directory
 - **2-str_concat.c**: a function that concatenates two strings
 - **3-alloc_grid.c**: a function that returns a pointer to a 2D array
 - **4-free_grid.c**: a function that frees a 2 dimensional grid previously created by `alloc_grid` function
-
+- **100-argstostr.c**: a function that concatenates all the arguments of your program
+- **101-strtow.c**: a function that splits a string into words
