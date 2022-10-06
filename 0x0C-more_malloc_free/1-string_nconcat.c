2f3d27644bf2383e6645bbb5c241d888abbeea60
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-10-06 09:43:59 +0000

    Add and Update files
    File Added:
    - 1-string_nconcat.c
    
    Updates:
    - main.h
    - README.md

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,59 @@
+#include <stdlib.h>
+#include "main.h"
+
+unsigned int len(char *);
+
+/**
+* string_nconcat - concatenates two strings
+* @s1: string 1
+* @s2: string2
+* @n: number of bytes to concat
+* Return: a pointer to a newly allocated space in memory
+* which contains s1, followed by the first n bytes of s2
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *p, i, j;
+
+	if (!s2)
+		s2 = "";
+
+	if (n >= len(s2))
+	{
+		p = malloc(sizeof(char) * (len(s1) + len(s2) + 1));
+		n = len(s2);
+	}
+	else
+		p = malloc(sizeof(char) * (len(s1) + n + 1));
+	if (!p)
+		return (NULL);
+
+	i = 0;
+	while ((*(p + i) = *(s1 + i)) != '\0')
+		++i;
+	j = 0;
+	while (n--)
+	{
+		*(p + i) = *(s2 + j);
+		++i;
+		++j;
+	}
+
+	return (p);
+}
+
+
+/**
+* len - returns the length of a string
+* @s: string argument
+* Return: length
+*/
+unsigned int len(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i))
+		++i;
+	return (i);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -5,3 +5,5 @@ Below is a list of files in this directory.
 
 ---
 - **0-malloc_checked.c**: a function that allocates memory using malloc
+- **1-string_nconcat.c**: a function that concatenates two strings
+
