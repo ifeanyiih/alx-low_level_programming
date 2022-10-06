f451be1a1bcf4d0f36591103eb2affd2275c3a49
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-10-06 11:59:28 +0000

    modified file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,7 +20,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (!s1)
 		s1 = "";
 	if (n >= len(s2))
-		n = len(s2);
+		n = len(s2) + 1;
 
 	p = malloc(sizeof(char) * (len(s1) + n + 1));
 	if (!p)
