c8b4424245f4f5e650d78fe60af603cf7b0aaf67
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-10-10 21:43:13 +0000

    modified file

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -26,6 +26,16 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		}
 		++i;
 	}
+	i = 0;
+	while (*(owner + i))
+	{
+		if (!isalpha(*(owner + i)))
+		{
+			printf("%s\n", "Error");
+			exit(98);
+		}
+		++i;
+	}
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
