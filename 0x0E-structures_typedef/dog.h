e62524b2a6b9b513da335cb6e57154be2c0ea597
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-10-10 16:33:24 +0000

    modified file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,6 +18,9 @@ struct dog
 void init_dog(struct dog *, char *, float, char *);
 void print_dog(struct dog *);
 
+/**
+* dog_t - Typedef for struct dog
+*/
 typedef struct dog dog_t;
 
 dog_t *new_dog(char *name, float age, char *owner);
