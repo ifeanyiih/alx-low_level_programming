5f34688c63c6cba6bfd2d604a5d69a8a6bd99ade
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-10-10 22:13:24 +0000

    modified file

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,7 +13,13 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (!d)
 		exit(98);
-	d->name = name;
+	if (name == NULL)
+		d->name = "(nil)";
+	else
+		d->name = name;
 	d->age = age;
-	d->owner = owner;
+	if (owner == NULL)
+		d->owner = "(nil)";
+	else
+		d->owner = owner;
 }
