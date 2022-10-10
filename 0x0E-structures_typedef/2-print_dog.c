a5a7022957dd46a92128e9858ab4b975c9048537
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-10-10 14:39:58 +0000

    modified file

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,10 +9,7 @@
 void print_dog(struct dog *d)
 {
 	if (!d)
-	{
-		printf("%s\n", "Error");
 		exit(98);
-	}
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
