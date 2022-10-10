30cd78179999acea73900f13e82cf572f6699fdc
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-10-10 22:29:04 +0000

    modified file

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,4 @@
 #include "dog.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -15,22 +14,17 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	char *name_cpy, *owner_cpy;
 	unsigned int i;
 
 	dog = malloc(sizeof(dog_t));
 	if (!dog)
 		exit(98);
 	i = 0;
-	name_cpy = NULL;
-	while ((*(name_cpy + i) = *(name + i)) != '\0')
+	while ((*(dog->name + i) = *(name + i)) != '\0')
 		;
 	i = 0;
-	owner_cpy = NULL;
-	while ((*(owner_cpy + i) = *(owner + i)) != '\0')
+	while ((*((*dog).owner + i) = *(owner + i)) != '\0')
 		;
-	dog->name = name_cpy;
 	dog->age = age;
-	(*dog).owner = owner_cpy;
 	return (dog);
 }
