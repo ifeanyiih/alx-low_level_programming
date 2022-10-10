313c1fde7ee7277454b80358f3e1b450a015bd35
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-10-10 22:39:15 +0000

    modified file

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -21,10 +21,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 		exit(98);
 	i = 0;
 	while ((*(dog->name + i) = *(name + i)) != '\0')
-		;
+		++i;
 	i = 0;
 	while ((*((*dog).owner + i) = *(owner + i)) != '\0')
-		;
+		++i;
 	dog->age = age;
 	return (dog);
 }
