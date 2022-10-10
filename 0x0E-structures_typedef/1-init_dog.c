02821823e13a4b33cf86e322208846a339f77be7
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-10-10 21:51:47 +0000

    modified file

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <ctype.h>
+#include <stdio.h>
 
 /**
 * init_dog - initializes a dog object
@@ -11,24 +11,8 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	unsigned int i;
-
 	if (!d)
 		exit(98);
-	i = 0;
-	while (*(name + i))
-	{
-		if (!isalnum(*(name + i)))
-			exit(98);
-		++i;
-	}
-	i = 0;
-	while (*(owner + i))
-	{
-		if (!isalnum(*(owner + i)))
-			exit(98);
-		++i;
-	}
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
