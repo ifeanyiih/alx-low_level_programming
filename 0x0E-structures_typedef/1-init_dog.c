da50b862d944903064363c653b61aac2f9315121
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-10-10 14:42:22 +0000

    modified file

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
 * init_dog - initializes a dog object
@@ -9,9 +10,9 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (!d)
+		exit(98);
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
-	free(name);
-	free(owner);
 }
