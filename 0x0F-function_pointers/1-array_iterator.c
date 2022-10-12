24990ed32f049dc8d9a51788f1370627e736ee62
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-10-12 15:05:16 +0000

    modified file

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,10 +13,14 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	i = 0;
-	while (i < size)
+	if (!(array == NULL || size <= 0 || action == NULL))
 	{
-		(*action)(array[i]);
-		++i;
+
+		i = 0;
+		while (i < size)
+		{
+			(*action)(array[i]);
+			++i;
+		}
 	}
 }
