f069c2ff14f1dfe4eef2e5f26195a3f760ad81b7
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-10-12 09:40:14 +0000

    Add and Update files
    Adds:
    - 2-int_index.c
    
    Updates:
    - function_pointers.h
    - README.md

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+
+/**
+* int_index - searches for an integer
+* @array: an array to search
+* @size: size of the array
+* @cmp: comparation function
+* Return: -1 if no match, -1 if size <= 0,
+* index of element otherwise
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	size_t i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (size <= 0)
+			return (-1);
+		if (!(*cmp)(array[i]))
+			return (i);
+		return (-1);
+	}
+}
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -6,3 +6,4 @@ Below is a list of files in this directory and a brief descriptions.
 ---
 - **0-print_name.c**: a function that prints a name
 - **1-array_iterator.c**: a function that iterates an array and executes a given function on all its elements
+- **2-int_index.c**: a function that searches for an integer
