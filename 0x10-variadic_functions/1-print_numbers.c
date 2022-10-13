e9077d50be68a08474cd2f3422db918f18f4f819
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-10-13 14:22:30 +0000

    Add and Update files
    Adds:
    - 1-print_numbers.
    
    Updates:
    - variadic_functions.h
    - README.md

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdargs.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+* print_numbers - prints numbers followed by a new line
+* @separator: string to be printed between numbers
+* @n: integer argument
+* @...: rest of the parameters
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	int i;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; ++i)
+	{
+		printf("%d%s", va_arg(ap, int), (separator == NULL) ? "" : separator);
+	}
+	_putchar('\n');
+
+	va_end(ap);
+}
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -5,3 +5,5 @@ Below is a list of files in this directory.
 
 ---
 - **0-sum_them_all.c**: a function that returns the sum of all its parameters
+- **1-print_numbers.c**: a function that prints numbers followed by a new line
+
