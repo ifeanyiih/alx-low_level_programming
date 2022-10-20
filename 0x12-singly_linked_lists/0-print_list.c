54f96c549f4140c5b6439844bb8f82e87cb02315
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-10-20 20:13:18 +0000

    modified file

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+size_t next(list_t *n);
+
 /**
 * print_list - prints all the element of a list
 * @h: a pointer to a type of list_t
@@ -9,14 +11,26 @@
 */
 size_t print_list(const list_t *h)
 {
+	printf("[%d] %s\n", h->len, (h->str == NULL) ? "(nil)" : h->str);
+	return (1 + next(h->next));
+}
+
+/**
+* next - traverse a list
+* @n: the list to traverse
+* Return: the number of nodes
+*/
+size_t next(list_t *n)
+{
 	size_t i;
 
-	i = 0;
-	while (h->next != NULL)
+	i = 1;
+	printf("[%d] %s\n", n->len, (n->str == NULL) ? "(nil)" : n->str);
+	while (n->next != NULL)
 	{
-		printf("[%d] %s\n", h->len, (h->str == NULL) ? "(nil)" : h->str);
+		printf("[%d] %s\n", n->len, (n->str == NULL) ? "(nil)" : n->str);
 		++i;
-		h = h->next;
+		n = n->next;
 	}
 	return (i);
 }
