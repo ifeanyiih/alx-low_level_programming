bbb842712d48e9e678e6e0dfb1accd5a0bc253a9
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-10-20 20:57:18 +0000

    add and modify files

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+* add_node - adds a new node at the beginning of a list
+* @head: the begining of a list
+* @str: string to be duplicated
+* Return: address of new element, otherwise NULL
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *l;
+	unsigned int i;
+	
+	l = malloc(sizeof(list_t));
+	if (l == NULL)
+		return (NULL);
+	i = 0;
+	while (*(str + i))
+		++i;
+	l->len = i;
+	l->str = strdup(str);
+	l->next = *head;
+	*head = l;
+	return (*head);
+}
