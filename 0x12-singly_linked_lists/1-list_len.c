a50a2b893396a5698f7e989962aa4dac804ca3de
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-10-20 20:32:02 +0000

    modify and add files

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+size_t next(list_t *n);
+
+/**
+* list_len - returns the number of elements in a linked list
+* @h: a pointer to a linked list
+* Return: number of elements
+*/
+size_t list_len(const list_t *h)
+{
+	if (h == NULL)
+		return (0);
+	return (1 + next(h->next));
+}
+
+/**
+* next - moves through a linked list
+* @n: a pointer to a list element
+* Return: number of elements
+*/
+size_t next(list_t *n)
+{
+	size_t i;
+
+	if (n == NULL)
+		return (0);
+	i = 1;
+	while(n->next != NULL)
+	{
+		++i;
+		n = n->next;
+	}
+	return (i);
+}
+
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stddef.h>
+#include <stdio.h>
 int _putchar(char c);
 
 /**
@@ -19,4 +20,5 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 #endif
