1301d9a9d3fb5884131f24b73f8ded4bee85ec92
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-10-24 14:47:59 +0000

    Add 1, modify 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+* free_listint - frees a listint_t list
+* @head: pointer to the list head
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *node, *copy;
+
+	if (*head != NULL)
+	{
+		copy = *head;
+		while (copy->next != NULL)
+		{
+			node = copy;
+			copy = copy->next;
+			free(node);
+		}
+		free(copy);
+		*head = NULL;
+	}
+}
