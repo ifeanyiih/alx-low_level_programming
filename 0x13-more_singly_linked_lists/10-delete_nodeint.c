dd749c0ce14c281824d4582182d5957d5bafde00
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-10-25 00:23:13 +0000

    modified file

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -23,7 +23,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	else
 	{
-		while ((idx < index - 1) && copy != NULL)
+		while ((idx < index - 1) && (copy != NULL))
 		{
 			copy = copy->next;
 			++idx;
