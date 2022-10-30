88815ebdc9df24f9a73f47d0846a15c3985648ba
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-10-30 01:50:05 +0000

    modified file

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,5 +1,8 @@
 #include "lists.h"
 
+listint_t *check_loop(listint_t *head);
+listint_t *start_loop(listint_t *p, listint_t *head);
+
 /**
 * print_listint_safe - prints a listint_t linked list
 * @head: the start of the list
@@ -7,19 +10,78 @@
 */
 size_t print_listint_safe(const listint_t *head)
 {
-	listint_t *cpy;
+	listint_t *cpy, *sl;
 	size_t nn;
 
 	if (head == NULL)
 		return (0);
-	nn = 1;
-	printf("[%p] %d\n", (void *)head, head->n);
 	cpy = head->next;
-	while (cpy != NULL)
+	nn = 0;
+	sl = check_loop(cpy);
+	printf("[%p] %d\n", (void *)head, head->n);
+	if (sl)
 	{
-		++nn;
-		printf("[%p] %d\n", (void *)cpy, cpy->n);
-		cpy = cpy->next;
+		sl = start_loop(sl, cpy);
+		do {
+			++nn;
+			printf("[%p] %d\n", (void *)cpy, cpy->n);
+			cpy = cpy->next;
+		} while (cpy != sl->next);
+
+		while (cpy != sl)
+		{
+			++nn;
+			printf("[%p] %d\n", (void *)cpy, cpy->n);
+			cpy = cpy->next;
+		}
+	}
+	else
+	{
+		do {
+			++nn;
+			printf("[%p] %d\n", (void *)cpy, cpy->n);
+			cpy = cpy->next;
+		} while (cpy != NULL);
 	}
 	return (nn);
+}
+
+
+/**
+* check_loop - checks if a linked list contains a loop
+* @head: head of the linked list
+* Return: a poiner to a node in the linked list
+*/
+listint_t *check_loop(listint_t *head)
+{
+	listint_t *p = head;
+	listint_t *q = head;
+
+	while (p && q && q->next)
+	{
+		p = p->next;
+		q = q->next->next;
+		if (p == q)
+			return (p);
+	}
+	return (NULL);
+}
+
+
+/**
+* start_loop - finds the start of a loop in a linked list
+* @p: a node in the loop part of the list
+* @head: the head of the linked list
+* Return: a pointer to the start of the loop, or NULL
+*/
+listint_t *start_loop(listint_t *p, listint_t *head)
+{
+	listint_t *q = head;
+
+	while (p != q)
+	{
+		p = p->next;
+		q = q->next;
+	}
+	return (p);
 }
