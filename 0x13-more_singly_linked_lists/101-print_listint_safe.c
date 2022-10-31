d5d1d8984e4eaccdca247dd96526aa7cc0240252
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-10-31 02:26:30 +0000

    modified file

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,24 +1,96 @@
 #include "lists.h"
 
-/**
- * print_listint_safe - prints a listint_t linked list
- * @head: the head node
- *
- * Return: the number of nodes in the list
-*/
+listint_t *check_loop(listint_t *head);
+listint_t *start_loop(listint_t *p, listint_t *head);
 
+/**
+* print_listint_safe - prints a listint_t linked list
+* @head: the start of the list
+* Return: the number of nodes in the list
+*/
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t count = 0;
+	listint_t *node, *sl;
+	size_t nn;
 
-	if (head)
+	if (head == NULL)
+		return (0);
+	nn = 1;
+	printf("[%p] %d\n", (void *)head, head->n);
+	node = head->next;
+	if (node == head)
+		return (nn);
+	if (node->next == head)
 	{
-		count++;
-		printf("[%p] %d\n", (void *)head, head->n);
-		if (head > head->next)
-			count += print_listint_safe(head->next);
-		else
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+		++nn;
+		printf("[%p] %d\n", (void *)node, node->n);
+		return (nn);
 	}
-	return (count);
+	sl = check_loop(node);
+	if (sl)
+	{
+		sl = start_loop(sl, node);
+		while (node != sl->next)
+		{
+			printf("[%p] %d\n", (void *)node, node->n);
+			++nn;
+			node = node->next;
+		}
+		while (node != sl)
+		{
+			++nn;
+			printf("[%p] %d\n", (void *)node, node->n);
+			node = node->next;
+		}
+		printf("-> [%p] %d\n", (void *)sl, sl->n);
+	}
+	else
+	{
+		do {
+			++nn;
+			printf("[%p] %d\n", (void *)node, node->n);
+			node = node->next;
+		} while (node && node != head);
+	}
+	return (nn);
+}
+
+
+/**
+* check_loop - checks if a linked list contains a loop
+* @head: head of the linked list
+* Return: a poiner to a node in the linked list
+*/
+listint_t *check_loop(listint_t *head)
+{
+	listint_t *p = head;
+	listint_t *q = head;
+
+	while (p && q && q->next)
+	{
+		p = p->next;
+		q = q->next->next;
+		if (p == q)
+			return (p);
+	}
+	return (NULL);
+}
+
+
+/**
+* start_loop - finds the start of a loop in a linked list
+* @p: a node in the loop part of the list
+* @head: the head of the linked list
+* Return: a pointer to the start of the loop, or NULL
+*/
+listint_t *start_loop(listint_t *p, listint_t *head)
+{
+	listint_t *q = head;
+
+	while (p != q)
+	{
+		p = p->next;
+		q = q->next;
+	}
+	return (p);
 }
