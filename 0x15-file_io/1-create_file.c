757ec4b775e9e5fff7f8710df6e609553455e2c4
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-11-01 00:45:31 +0000

    modified file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	unsigned int i;
 
-	if (filename == NULL)
+	if (filename == NULL || text_content == NULL)
 		return (-1);
 	fd = open(filename, O_CREAT, PERM);
 	if (fd < 0)
