3716fc02897d63544d2871fd72e55720acf56e13
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-12-08 20:29:54 +0000

    Add and Update files

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a dlistint_t list
+ * @head: pointer to head of the list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *node;
+
+	while (head->next != NULL)
+	{
+		node = head;
+		head = head->next;
+		node->next = NULL;
+		free(node);
+		head->prev = NULL;
+	}
+	free(head);
+}
