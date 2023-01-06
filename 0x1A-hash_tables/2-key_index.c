ad7da529dfaa187d420de7aff398219d96f7b8b9
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2023-01-06 17:43:49 +0000

    Add file

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_table.h"
+
+/**
+ * key_index - returns the index based on the given key
+ * @key: the key to generate the index from
+ * @size: the size of the hash table array
+ * Return: index
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash;
+
+	hash = hash_djb2(key);
+	return (hash % size);
+}
