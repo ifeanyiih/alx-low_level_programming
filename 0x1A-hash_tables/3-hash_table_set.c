51ece11810e30f9c56eea9ad5a3685c3d58d8462
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2023-01-07 01:09:05 +0000

    Add file

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: the hash table
+ * @key: element key
+ * @value: element value
+ * Return: 1 on Success, otherwise 0
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	char *v_cpy;
+	hash_node_t *node;
+	unsigned int i;
+	unsigned long int index;
+
+	if ((char *)key == NULL)
+		return (0);
+	if (*key == '\0')
+		return (0);
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
+	node->key = (char *) key;
+	index = key_index((unsigned char *)key, ht->size);
+	i = 0;
+	while (value[i] != '\0')
+		++i;
+	v_cpy = malloc(sizeof(char) * (i + 1));
+	if (v_cpy == NULL)
+		return (0);
+	strcpy(v_cpy, (char *) value);
+	node->value = v_cpy;
+	node->next = ht->array[index];
+	ht->array[index] = node;
+	return (1);
+}
