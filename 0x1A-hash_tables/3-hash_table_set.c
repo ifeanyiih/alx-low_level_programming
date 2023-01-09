86828e031a5794f81e362b5330c01fa7151e5998
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2023-01-09 02:04:47 +0000

    fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,6 +11,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *node;
 	unsigned long int index;
+	char *sp;
 
 	if ((char *)value == NULL)
 		return (0);
@@ -25,11 +26,17 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	if (strcmp(key, "") == 0)
 		return (0);
-	node->key = strdup(key);
+	sp = strdup(key);
+	if (sp == NULL)
+		return (1);
+	node->key = sp;
 	index = key_index((unsigned char *)key, ht->size);
 	if (node->value != NULL)
 		free(node->value);
-	node->value = strdup(value);
+	sp = strdup(value);
+	if (sp == NULL)
+		return (1);
+	node->value = sp;
 	node->next = ht->array[index];
 	ht->array[index] = node;
 	return (1);
